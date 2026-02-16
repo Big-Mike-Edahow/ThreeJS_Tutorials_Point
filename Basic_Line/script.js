e523f46d245c434ef1cfed4e48602062115b416e
Big-Mike-Edahow <perigran_falcon@yahoo.com>
2026-02-15 18:53:19 -0700

    Repetition. Repetition. Repetition.

---
+++ Basic_Line/script.js
@@ -0,0 +1,68 @@
+// script.js
+
+import * as THREE from 'three'
+import { GUI } from 'three/addons/libs/lil-gui.module.min.js'
+
+const gui = new GUI()                                                       // Create a new GUI
+
+let width = window.innerWidth                                               // Set window size.
+let height = window.innerHeight
+
+const scene = new THREE.Scene()                                             // Create a new scene.
+scene.background = new THREE.Color(0x262626)
+
+const camera = new THREE.PerspectiveCamera(45, width / height, 0.1, 100)    // New perspective camera.
+camera.position.set(0, 0, 50)
+camera.lookAt(0, 0, 0)
+const camFolder = gui.addFolder('Camera')
+camFolder.add(camera.position, 'z', 10, 100)
+camFolder.open()
+
+const points = []                                                           // Lines.
+points.push(new THREE.Vector3(-10, 0, 0))
+points.push(new THREE.Vector3(0, -20, 0))
+points.push(new THREE.Vector3(10, 0, 0))
+const folders = [gui.addFolder('Poin 1'), gui.addFolder('Poin 2'), gui.addFolder('Poin 3')]
+folders.forEach((folder, i) => {
+    folder.add(points[i], 'x', -30, 30, 1).onChange(redraw)
+    folder.add(points[i], 'y', -30, 30, 1).onChange(redraw)
+    folder.add(points[i], 'z', -30, 30, 1).onChange(redraw)
+    folder.open()
+})
+const geometry = new THREE.BufferGeometry().setFromPoints(points)
+const material = new THREE.LineBasicMaterial({
+    color: 0x00FF00,
+    linewidth: 10
+})
+const line = new THREE.Line(geometry, material)
+line.position.set(0, 10, 0)
+scene.add(line)
+function redraw() {
+    let newGeometry = new THREE.BufferGeometry().setFromPoints(points)
+    line.geometry.dispose()
+    line.geometry = newGeometry
+}
+
+window.addEventListener('resize', () => {                                   // Responsiveness.     
+    width = window.innerWidth
+    height = window.innerHeight
+    camera.aspect = width / height
+    camera.updateProjectionMatrix()
+    renderer.setSize(window.innerWidth, window.innerHeight)
+    renderer.render(scene, camera)
+})
+
+const renderer = new THREE.WebGLRenderer()                                  // Renderer.
+renderer.setSize(width, height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+function animate() {                                                        // Main animation loop.
+    requestAnimationFrame(animate)                                          // Set up the next frame.
+    renderer.render(scene, camera)                                          // Render the scene.
+}
+
+const container = document.querySelector('#threejs-container')              // Append canvas to HTML
+container.append(renderer.domElement)
+
+renderer.render(scene, camera)                                              // Render the scene.
+requestAnimationFrame(animate)                                              // Start the animation.       
